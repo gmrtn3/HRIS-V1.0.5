b1fa55f680486af27da106a51b96a6b22e819f33
Bulbasar <gerardmartin670@gmail.com>
2023-09-07 14:57:58 +0800

    HRIS V1.0.6

--- Dashboard.php
+++ Dashboard.php
@@ -60,16 +60,16 @@
    } 
 //    // for payroll holiday rule for holiday computations END
 
-//    $query_settings = "SELECT * FROM settings_company_tb";
-//    $query_run_Settings = mysqli_query($conn, $query_settings);
+   $query_settings = "SELECT * FROM settings_company_tb";
+   $query_run_Settings = mysqli_query($conn, $query_settings);
 
-//    if (mysqli_num_rows($query_run_Settings) <= 0){
-//        $row_settings = mysqli_fetch_assoc($query_run_Settings);
+   if (mysqli_num_rows($query_run_Settings) <= 0){
+       $row_settings = mysqli_fetch_assoc($query_run_Settings);
 
-//        $query_settings = "INSERT INTO settings_company_tb (`col_salary_settings`)
-//        VALUES ('Fixed Salary')";
-//        $query_run_Settings = mysqli_query($conn, $query_settings);   
-//    }
+       $query_settings = "INSERT INTO settings_company_tb (`col_salary_settings`)
+       VALUES ('Fixed Salary')";
+       $query_run_Settings = mysqli_query($conn, $query_settings);   
+   }
 
 
 
