b368254530a323c92a0d2d497e3a83bd128c5267
Bulbasar <gerardmartin670@gmail.com>
2023-09-07 14:24:32 +0800

    HRIS V1.0.6

--- login.php
+++ login.php
@@ -40,7 +40,7 @@ if(isset($_POST['signIn'])){
                 $_SESSION['role'] = $row['role'];
                 
                 if($row['role'] == 'admin'){
-                    header("Location: Dashboard.php"); // Redirect to admin dashboard
+                    header("Location: asdDashboard.php"); // Redirect to admin dashboard
                     exit();
                 } else if($row['role'] == 'Employee'){
                     header("Location: Dashboard.php"); // Redirect to employee dashboard
