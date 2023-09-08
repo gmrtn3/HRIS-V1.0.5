4731cee7efbfa5ff8fa862bb496b51c51a3f14dd
Bulbasar <gerardmartin670@gmail.com>
2023-09-08 10:43:57 +0800

    HRIS V1.0.6

--- SpecialFolders/Models/TimeOut.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Db\Model;
-require_once '../Databases/Model.php';
-class TimeOut extends Model{
-    protected $table = 'attendance_time_out';
-    protected $primaryKey = 'time_out_personId';
-}
